a89402115c452527d9318797069932d12b8e6b9c
CARLOS HENRIQUE <ch8529720@gmail.com>
2023-04-25 07:37:22 -0300

    Create atividade 1.c

---
+++ atividade 1.c
@@ -0,0 +1,50 @@
+	
+
+ #include <stdio.h>
+ int main (){
+ 	
+ 	int num1,num2,soma;
+ 	
+ 	printf(" Por favor  usuario infome  o primeiro  valor : " );
+ 	scanf ("%d", &num1);
+
+	
+	printf(" Por favor usuario  infome  o segundo valor : " );
+ 	scanf ("%d", &num2);
+ 	
+ 	
+ 	soma = num1 + num2;
+ 	
+ 	
+ 	printf ("o resultado da soma e igual a %d ",soma);
+ 	
+ 	
+ 	
+ 	return 0;
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ }
