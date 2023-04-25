2d6320b0dc978fb46c4b1fcea536099f26b66403
CARLOS HENRIQUE <130611356+carlosbode029@users.noreply.github.com>
2023-04-25 20:17:21 -0300

    Update atividade 10.c

--- atividade 10.c
+++ atividade 10.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main(){
-		char nome[20];
-		int Idade;
-	    
-		printf("Informe seu nome:   ");
-	    scanf("%s",nome);
-		system("cls");
-		printf("Informe sua idade:   ");
-	    scanf("%d",Idade);
-	    system("cls");
-	    printf("Seja bem vindo %c", nome);
-	    
-	    
-		
-	    
-	
-	
+
+int main() {
+    char nome[20];
+    int idade;
+
+    printf("Informe seu nome: ");
+    scanf("%s", nome);
+    system("cls");
+    printf("Informe sua idade: ");
+    scanf("%d", &idade);
+    system("cls");
+    printf("Seja bem vindo %s", nome);
+
+    return 0;
 }
+
+//O código tem como objetivo solicitar e armazenar o nome e a idade de um usuário e apresentar uma mensagem de boas-vindas com o nome fornecido
