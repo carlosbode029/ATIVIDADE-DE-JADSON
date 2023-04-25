9d46263545c3f91bbf678fa23bf17171abfc66a2
CARLOS HENRIQUE <ch8529720@gmail.com>
2023-04-25 07:37:12 -0300

    Create atividade 10.c

---
+++ atividade 10.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+		char nome[20];
+		int Idade;
+	    
+		printf("Informe seu nome:   ");
+	    scanf("%s",nome);
+		system("cls");
+		printf("Informe sua idade:   ");
+	    scanf("%d",Idade);
+	    system("cls");
+	    printf("Seja bem vindo %c", nome);
+	    
+	    
+		
+	    
+	
+	
+}
