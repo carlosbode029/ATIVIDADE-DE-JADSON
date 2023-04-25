29908ad7f2f1d745c858271e54c5c925a1924720
CARLOS HENRIQUE <130611356+carlosbode029@users.noreply.github.com>
2023-04-25 20:14:01 -0300

    Update Atividade 7.cpp

--- Atividade 7.cpp
+++ Atividade 7.cpp
@@ -16,3 +16,11 @@ int main() {
     return 0;
 }
 
+//Este é um programa em C que solicita que o usuário digite sua idade e, em seguida, verifica se a idade é maior ou igual a 18 anos. Se a idade for maior ou igual a 18,
+//o programa exibe uma mensagem indicando que o usuário é maior de idade. Caso contrário, exibe uma mensagem indicando que o usuário é menor de idade. O programa utiliza 
+//a estrutura condicional "if-else" para realizar a verificação e a função scanf para obter entrada de dados. Ele é uma demonstração simples do uso de 
+//estruturas condicionais em C.
+
+
+
+
