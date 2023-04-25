3b6aebb06132a2334e3128e95d27879a6c4c35bf
CARLOS HENRIQUE <ch8529720@gmail.com>
2023-04-25 07:34:34 -0300

    Create atividade 2.c

---
+++ atividade 2.c
@@ -0,0 +1,26 @@
+
+ #include <stdio.h>
+ int main (){
+ 	
+ 	int raio;
+ 	
+ 	float 	 pi = 3.14, area;
+
+ 	
+ 	printf(" Por favor, informe  o valor do raio : " );
+ 	scanf ("%f", &raio);
+
+	area = pi * raio * raio;
+
+ 	printf ("a area do circulo sera de %d  ",raio);
+ 	
+ 	
+ 	return 0;
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+
+ }
