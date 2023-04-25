c2e466ac6307015d2049d909727781b214f44860
CARLOS HENRIQUE <130611356+carlosbode029@users.noreply.github.com>
2023-04-25 20:12:42 -0300

    Update Atividade 6.cpp

--- Atividade 6.cpp
+++ Atividade 6.cpp
@@ -13,3 +13,10 @@ int main() {
     
 }
 
+//Este é um programa em C que solicita que o usuário insira um número real, e em seguida, extrai e exibe sua parte inteira na tela. Ele usa a função scanf para
+//obter entrada e a função printf para exibir saída, e converte o número real em um inteiro usando casting. O programa é uma demonstração simples do uso de tipos de 
+//dados e conversões em C.
+
+
+
+
